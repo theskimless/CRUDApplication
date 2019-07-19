e9fed159bca31275fe6dfb847863fe402fae2366
Виталий <TheSkimless@gmail.com>
2019-07-19 19:28:34 +0300

    port changed

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 function _(data) { console.log(data) }
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const express = require("express");
 const app = express();
+
+// const http = require("http").Server(app);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(port, () => _(port));
+app.listen(PORT, () => _(PORT));
